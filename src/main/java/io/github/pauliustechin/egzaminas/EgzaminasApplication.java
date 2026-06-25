61158171e05ecef98f137716a8b490f9a294fbde
pauliustechin <pauliustechin@gmail.com>
2026-06-25 08:47:11 +0300

    first push

---
+++ src/main/java/io/github/pauliustechin/egzaminas/EgzaminasApplication.java
@@ -0,0 +1,13 @@
+package io.github.pauliustechin.egzaminas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgzaminasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgzaminasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pauliustechin/egzaminas/EgzaminasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pauliustechin.egzaminas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgzaminasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
